96668eb85677edc4684a3f983d32e9e11855c966
Adolfo Neto <adolfo.usp@gmail.com>
2014-09-09 11:27:52 -0400

    project.clj added

---
+++ leinproject/project.clj
@@ -0,0 +1,8 @@
+(defproject kems2 "1.0.0-SNAPSHOT"
+  :description "KEMS2: a KE Multi-Strategy Prover"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :url "http://www2.dainf.ct.utfpr.edu.br/Members/adolfo/pesquisa/kems-2"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main ^:skip-aot kems2.core
+)
